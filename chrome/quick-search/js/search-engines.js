65f95436020152511066e3047feef75e046cba55
gu-wei-x <140672679+gu-wei-x@users.noreply.github.com>
2024-05-03 11:47:16 -0700

    Update icons (#7)

--- chrome/quick-search/js/search-engines.js
+++ chrome/quick-search/js/search-engines.js
@@ -6,14 +6,14 @@ export const SearchEngines = {
                 "type": "serp",
                 "title": "Search",
                 "url_temple": "https://www.bing.com/search?q=",
-                "icon": "-10px -68px;",
-                "show_query": "1",
+                "icon": "-62px -10px;",
+                "show_query": "1"
             },
             {
                 "type": "ai",
                 "title": "Ask Copilot",
                 "url_temple": "https://www.bing.com/search?showconv=1&sendquery=1&q=",
-                "icon": "-68px -10px;"
+                "icon": "-10px -114px;"
             }
         ]
     },
@@ -24,19 +24,19 @@ export const SearchEngines = {
                 "type": "serp",
                 "title": "Search",
                 "url_temple": "https://www.google.com/search?q=",
-                "icon": "-10px -10px;",
-                "show_query": "1",
+                "icon": "-10px -62px",
+                "show_query": "1"
             }
         ]
     },
-    "baidu": {
-        "name": "Baidu",
+    "duckduckgo": {
+        "name": "DuckDuckGo",
         "entries": [
             {
                 "type": "serp",
                 "title": "Search",
-                "url_temple": "https://www.baidu.com/s?wd=",
-                "icon": "-10px -10px;",
+                "url_temple": "https://duckduckgo.com/?va=g&t=hj&ia=web&q=",
+                "icon": "-114px -10px;",
                 "show_query": "1"
             }
         ]
--- chrome/quick-search/readme.txt
+++ chrome/quick-search/readme.txt
@@ -6,3 +6,5 @@ setup:
 1. cd to this folder
 2. npm install
 3. npm run build.
+
+sprite-generator: https://www.toptal.com/developers/css/sprite-generator
