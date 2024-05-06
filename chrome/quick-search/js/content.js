0ba3d24c86849df53e3938aaa1e5a229b3a02928
gu-wei-x <140672679+gu-wei-x@users.noreply.github.com>
2024-05-06 13:28:20 -0700

    Load menu theme from css file (#9)
    Co-authored-by: Wei Gu <guw@microsoft.com>

--- chrome/quick-search/js/content.js
+++ chrome/quick-search/js/content.js
@@ -1,66 +1,70 @@
 import { OnSelectedMenu } from "./on-selected-menu.js"
 import { Settings } from "./settings.js";
 
-var selectMenu;
-document.onselectstart = (e) => {
-    if (!selectMenu) {
-        selectMenu = new OnSelectedMenu();
-    }
-};
+const theme_url = chrome.runtime.getURL('css/menu_theme.css');
+OnSelectedMenu.loadTheme(theme_url).then(() => {
+    var selectMenu;
+    document.onselectstart = (e) => {
+        if (!selectMenu) {
+            selectMenu = new OnSelectedMenu();
+        }
+    };
 
-document.addEventListener("mouseup", async (e) => {
-    if (!selectMenu) {
-        selectMenu = new OnSelectedMenu();
-    }
+    document.addEventListener("mouseup", async (e) => {
+        if (!selectMenu) {
+            selectMenu = new OnSelectedMenu();
+        }
 
-    if (e.target && e.target.id == OnSelectedMenu.getContainerID()) {
-        selectMenu.hide();
-        return;
-    }
-
-    var selection = document.getSelection()
-    const selectedText = selection.toString();
-    if (selection.rangeCount < 1 || !selectedText) {
-        if (selectMenu) {
+        if (e.target && e.target.id == OnSelectedMenu.getContainerID()) {
             selectMenu.hide();
-        }
-        return;
-    }
-
-    if (selectMenu) {
-        var selectedRect = window.getSelection().getRangeAt(0).getBoundingClientRect();
-        var top = (e.clientY) + "px";
-        var left = (e.clientX) + "px";
-        if (selectedRect.height > 0) {
-            var domRelative = document.body.parentNode.getBoundingClientRect();
-            top = (selectedRect.bottom - domRelative.top) + "px";
-            left = (selectedRect.left - domRelative.left) + "px";
+            return;
         }
 
-        selectMenu.setPosition(top, left);
-
-        // magic number.
-        const queryMaxLength = 1354;
-        var enSelectedText = encodeURIComponent(selectedText);
-        if (enSelectedText.length > queryMaxLength) {
-            enSelectedText = enSelectedText.substring(0, queryMaxLength);
+        var selection = document.getSelection()
+        const selectedText = selection.toString();
+        if (selection.rangeCount < 1 || !selectedText) {
+            if (selectMenu) {
+                selectMenu.hide();
+            }
+            return;
         }
-    
-        let content = [];
-        const defaultSearchEntry = await Settings.getDefaultSearchEntry();
-        if (defaultSearchEntry && defaultSearchEntry.entries) {
-            defaultSearchEntry.entries.forEach(entry => {
-                content.push({
-                    name: defaultSearchEntry.name,
-                    type: entry.type,
-                    content: entry["show_query"] ? selectedText : "",
-                    title: entry.title,
-                    url: `${entry.url_temple}${selectedText}`
+
+        if (selectMenu) {
+            var selectedRect = window.getSelection().getRangeAt(0).getBoundingClientRect();
+            var top = (e.clientY) + "px";
+            var left = (e.clientX) + "px";
+            if (selectedRect.height > 0) {
+                var domRelative = document.body.parentNode.getBoundingClientRect();
+                top = (selectedRect.bottom - domRelative.top) + "px";
+                left = (selectedRect.left - domRelative.left) + "px";
+            }
+
+            selectMenu.setPosition(top, left);
+
+            // magic number.
+            const queryMaxLength = 1354;
+            var enSelectedText = encodeURIComponent(selectedText);
+            if (enSelectedText.length > queryMaxLength) {
+                enSelectedText = enSelectedText.substring(0, queryMaxLength);
+            }
+
+            let content = [];
+            const defaultSearchEntry = await Settings.getDefaultSearchEntry();
+            if (defaultSearchEntry && defaultSearchEntry.entries) {
+                defaultSearchEntry.entries.forEach(entry => {
+                    content.push({
+                        name: defaultSearchEntry.name,
+                        type: entry.type,
+                        content: entry["show_query"] ? selectedText : "",
+                        title: entry.title,
+                        url: `${entry.url_temple}${selectedText}`
+                    });
                 });
-            });
+            }
+            
+            selectMenu.setSelectedText(selectedText);
+            selectMenu.setContent(content);
+            selectMenu.show();
         }
-
-        selectMenu.setContent(content);
-        selectMenu.show();
-    }
+    });
 });
--- chrome/quick-search/js/on-selected-menu.js
+++ chrome/quick-search/js/on-selected-menu.js
@@ -4,11 +4,7 @@ import { Settings } from "./settings.js";
 export class OnSelectedMenu {
     static containerID = "quick-serch-selected-menu";
     static #icon_url = chrome.runtime.getURL("images/icons.png");
-    static #styles = '.selected-menu-box { z-index: 10000;position: absolute;cursor: pointer;border: 0;background: #FFF;box-shadow: 0 2px 10px 0 rgba(0, 0, 0, .1);border-radius: 6px;padding: 10px 15px 9px 16px;display: block;}'
-        + '.selected-menu-box a,.selected-menu-box a:hover,.selected-menu-box a:visited {text-decoration: none;color: #333;float: left;}'
-        + '.selected-menu-box a:hover {background-color: gray;}'
-        + '.selected-menu-box span {overflow: hidden;float: left;font-family: Arial, MicrosoftYaHei;white-space: nowrap;text-overflow: ellipsis;max-width: 64px;line-height: 32px;}'
-        + '.selected-menu-box .c-icon {float: left;transform: scale(.5); width: 32px;height: 32px;}';
+    static #themeCss = '';
 
     constructor() {
         this.initialize();
@@ -27,6 +23,10 @@ export class OnSelectedMenu {
         this.shadowRoot.appendChild(content.cloneNode(true));
         document.body.appendChild(this.menuContainer);
         // TODO: bind actions.
+    }
+
+    setSelectedText(selectedText) {
+        this.selectedText = selectedText;
     }
 
     setContent(content) {
@@ -57,7 +57,8 @@ export class OnSelectedMenu {
     #render() {
         var slot = this.shadowRoot.querySelector('slot');
         slot.replaceChildren();
-        if (this.content) {
+        if (this.selectedText && this.content) {
+            slot.appendChild(OnSelectedMenu.#createSelectedTextItem(this.selectedText));
             this.content.forEach(item => {
                 let menuItem = OnSelectedMenu.#createMenuItem(item);
                 slot.appendChild(menuItem);
@@ -69,17 +70,32 @@ export class OnSelectedMenu {
         return OnSelectedMenu.containerID;
     }
 
+    static loadTheme(theme_url) {
+        if (!theme_url) {
+            return;
+        }
+
+        return fetch(theme_url)
+            .then((response) => response.text())
+            .then((themeCss) => {
+                if (themeCss) {
+                    OnSelectedMenu.#themeCss = themeCss;
+                }
+            });
+    }
+
+    static #createSelectedTextItem(selectedText) {
+        const textItem = document.createElement("span");
+        const textContent = document.createTextNode(selectedText);
+        textItem.appendChild(textContent);
+        return textItem;
+    }
+
     static #createMenuItem(item) {
         var menuItem = document.createElement("a");
         menuItem.title = item.title;
         menuItem.setAttribute("target", "_blank");
         menuItem.setAttribute("href", item.url);
-        if (item.content) {
-            var content = document.createElement("span");
-            var textContent = document.createTextNode(item.content);
-            content.appendChild(textContent);
-            menuItem.appendChild(content);
-        }
 
         let icon_class = `c-icon-${item.name.toLowerCase()}-${item.type}`;
         let icon = document.createElement("i");
@@ -95,17 +111,8 @@ export class OnSelectedMenu {
             const menuTemplate = document.createElement("template");
             menuTemplate.id = "custom-selected-menu-template";
 
-            let styleContent = OnSelectedMenu.#styles;
-            for (const [key, value] of await Settings.getAllSearchEngines()) {
-                if (value.entries) {
-                    value.entries.forEach(entry => {
-                        styleContent += `.selected-menu-box .c-icon-${key}-${entry.type}{background: url(${OnSelectedMenu.#icon_url}) no-repeat ${entry.icon}}`;
-                    });
-                }
-            }
-
             const style = document.createElement("style");
-            style.innerText = styleContent;
+            style.innerText = OnSelectedMenu.#themeCss;
             menuTemplate.content.appendChild(style);
 
             const menu = document.createElement("div");
